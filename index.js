c6c132a23c48860bd50908b6edf78984763d1ab3
Carlos Alba <calba5141114@Carloss-MacBook-Pro.local>
2018-08-28 16:02:31 -0700

    config

---
+++ index.js
@@ -0,0 +1,11 @@
+const config = require('./config');
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("Hello World");
+})
+
+app.listen(config.port,()=>{
+    console.log(`App running on ${config.port}`);
+})
